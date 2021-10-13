06736fb96e708c4080dbe360e2b0c6d488bfb317
javiercorrea1255 <92273977+javiercorrea1255@users.noreply.github.com>
2021-10-12 19:55:54 -0500

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,4 +45,23 @@
             </div>
         </form>
     </x-jet-authentication-card>
+     <script type="text/javascript">
+        function callbackThen(response){
+            // read HTTP status
+            console.log(response.status);
+        
+            // read Promise object
+            response.json().then(function(data){
+                console.log(data);
+            });
+        }
+        function callbackCatch(error){
+            console.error('Error:', error)
+        }
+        </script>
+        
+        {!! htmlScriptTagJsApi([
+            'callback_then' => 'callbackThen',
+            'callback_catch' => 'callbackCatch'
+        ]) !!}
 </x-guest-layout>
