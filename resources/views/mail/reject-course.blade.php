fdb88670824e296bedd1bd931ef0850f3cd9b377
javiercorrea1255 <92273977+javiercorrea1255@users.noreply.github.com>
2021-10-14 21:38:05 -0500

    Update reject-course.blade.php

--- resources/views/mail/reject-course.blade.php
+++ resources/views/mail/reject-course.blade.php
@@ -7,10 +7,10 @@
     <title>Document</title>
 </head>
 <body>
-    <h1 style="color:rgb(241, 76, 76)"><b>Oh no!</b></h1>
-    <p>Your course <b>{{ $course->title }}</b> have been rejected!</p>
+    <h1 style="color:rgb(241, 76, 76)"><b>Atención</b></h1>
+    <p>Estimado instructor de Directo al Campo, lamentamos informarle que su curso <b>{{ $course->title }}</b> ha sido rechazado, por favor corrija las observaciónes que se adjuntan y vuelva a enviar el curso para su revisión</p>
     <hr>
-    <h3>Details:</h3>
+    <h3>Detalles:</h3>
     <p>{!! $course->observation->body !!}</p>
 </body>
 </html>
