3e140d79cd271a2887c181ceef364d3247be8e16
Javier Correa <javiercorrea1255@gmail.com>
2021-10-18 14:58:57 -0500

    cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Directo al Campo</title>
+</head>
+<body>
+    
+
 <x-app-layout>
 
 <title>Directo al Campo</title>
@@ -120,3 +131,5 @@
 
 </x-app-layout>
 
+</body>
+</html>
