5d9f1b3e38fe8b4f96a0f98e459eb9017cc6d49b
Javier Correa <javiercorrea1255@gmail.com>
2021-10-19 12:21:31 -0500

    PASARELADEPAGO

--- resources/views/payment/checkout.blade.php
+++ resources/views/payment/checkout.blade.php
@@ -1,26 +1,26 @@
 <x-app-layout>
     <div class="max-w-2xl mx-auto pt-24 sm:px-6 lg:px-8 py-12 text-blue-400">
         <div class="card p-2">
-            <h1 class="text-2xl text-center font-bold uppercase">Detalhes do Pedido</h1>
+            <h1 class="text-2xl text-center font-bold uppercase">Detalles del pedido</h1>
             <hr class="mt-2 mb-6">
             <article class="flex items-center">
                 <img class="h-20 w-20 mr-2 object-cover rounded-md" src="{{Storage::url($course->image->url)}}" alt="">
                 <h1 class="text-lg">{{$course->title}}</h1>
                 <p class="text-lg font-bold ml-auto">
-                    R$ {{number_format($course->price->value, 2, ',', '.')}}
+                    MXN$ {{number_format($course->price->value, 2, ',', '.')}}
                 </p>
             </article>
  
             <div class="flex justify-end">
                 <a href="{{route('payment.pay', $course)}}" class="btn btn-info">
                     <div class="flex items-center">
-                        <p>Confirmar Compra com</p>
+                        <p>CONFIRMAR COMPRA</p>
                         <i class="fab fa-cc-paypal text-4xl ml-2"></i>
                     </div>
                 </a>
             </div>
  
-            <div class="card my-8 mx-12 p-4 text-sm text-blue-300">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor molestias soluta id accusamus qui quibusdam suscipit fugiat possimus harum molestiae.
+            <div class="card my-8 mx-12 p-4 text-sm text-blue-300">El presente pago está a cargo de la pasarela de Pago 'Paypal, cualquier duda, queja o responsabilidad en la gestión y aprobación de los pagos debe ser aclarada con Paypal, no con Directoalcampo.org.
                 <a href="" class="text-red-300 font-bold">Termos e Condições</a>
             </div>
         </div>
