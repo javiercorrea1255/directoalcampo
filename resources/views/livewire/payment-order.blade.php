25abb87abe60dc1a15928a67f34bf89723af503f
Javier Correa <javiercorrea1255@gmail.com>
2021-10-18 23:00:02 -0500

    payment3

--- resources/views/livewire/payment-order.blade.php
+++ resources/views/livewire/payment-order.blade.php
@@ -58,7 +58,7 @@
                 // Successful capture! For dev/demo purposes:
                   console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
                   var transaction = orderData.purchase_units[0].payments.captures[0];
-               
+                  alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
   
               // When ready to go live, remove the alert and show a success message within this page. For example:
               // var element = document.getElementById('paypal-button-container');
