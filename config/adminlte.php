0f4876d9c2cc88e4fe3ae0d6432e2091149a109a
Javier Correa <javiercorrea1255@gmail.com>
2021-10-18 20:51:38 -0500

    vendor

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,8 +45,8 @@ return [
     |
     */
 
-    'logo' => '<b>Course</b>Free',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo' => '<b>Directo al</b>Campo',
+    'logo_img' => 'vendor/adminlte/dist/img/dc.jpg',
     'logo_img_class' => 'brand-image img-circle elevation-1',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
