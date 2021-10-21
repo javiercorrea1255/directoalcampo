6732a0925544889b0c24845ca80ae6f495b1725d
Directoalcampo <javiercorrea1255@gmail.com>
2021-10-21 17:19:50 -0500

    22

--- resources/views/livewire/courses-coments.blade.php
+++ resources/views/livewire/courses-coments.blade.php
@@ -10,33 +10,7 @@
                 <div class="flex-1 bg-gray-50 rounded shadow-inner p-3">
                     <textarea wire:model="comment" rows="3" class="form-input" placeholder="Escriba una reseña del curso..."></textarea>
                     <div class="flex justify-between items-center gap-3">
-                        <ul class="flex text-lg">
-                            <li class="mr-1 cursor-pointer" wire:click="$set('rating', 1)">
-                                <svg class="text-{{ $rating >= 1 ? 'yellow' : 'gray' }}-300" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-                                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                                </svg>
-                            </li>
-                            <li class="mr-1 cursor-pointer" wire:click="$set('rating', 2)">
-                                <svg class="text-{{ $rating >= 2 ? 'yellow' : 'gray' }}-300" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-                                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                                </svg>
-                            </li>
-                            <li class="mr-1 cursor-pointer" wire:click="$set('rating', 3)">
-                                <svg class="text-{{ $rating >= 3 ? 'yellow' : 'gray' }}-300" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-                                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                                </svg>
-                            </li>
-                            <li class="mr-1 cursor-pointer" wire:click="$set('rating', 4)">
-                                <svg class="text-{{ $rating >= 4 ? 'yellow' : 'gray' }}-300" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-                                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                                </svg>
-                            </li>
-                            <li class="mr-1 cursor-pointer" wire:click="$set('rating', 5)">
-                                <svg class="text-{{ $rating == 5 ? 'yellow' : 'gray' }}-300" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-                                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                                </svg>
-                            </li>
-                        </ul>
+                    
                         <div>
                             <button type="button" class="btn btn-blue" wire:click="store">Publicar reseña</button>
                         </div>
