137189f84713ec123165f67492002cd605c51dd9
Directoalcampo <javiercorrea1255@gmail.com>
2021-10-21 16:57:58 -0500

    prueba2

--- app/Http/Livewire/CoursesComents.php
+++ app/Http/Livewire/CoursesComents.php
@@ -6,8 +6,17 @@ use Livewire\Component;
 
 class CoursesComents extends Component
 {
+    public $course_id;
+
+    public function mount(Course $course){
+        $this->course_id =$course->id;
+
+    }
+
     public function render()
     {
-        return view('livewire.courses-coments');
+        $course = Course::find($this->course_id);
+
+        return view('livewire.courses-coments', compact('course'));
     }
 }
