d7452fefae2cec6bf9f76b0b6185127340d4ef8a
Directoalcampo <javiercorrea1255@gmail.com>
2021-11-06 13:14:58 -0600

    chat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -166,6 +166,33 @@
         </div>
 
     </section>
+<!-- Messenger plugin de chat Code -->
+<div id="fb-root"></div>
+
+<!-- Your plugin de chat code -->
+<div id="fb-customer-chat" class="fb-customerchat">
+</div>
+
+<script>
+  var chatbox = document.getElementById('fb-customer-chat');
+  chatbox.setAttribute("page_id", "197438398980799");
+  chatbox.setAttribute("attribution", "biz_inbox");
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v12.0'
+    });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+</script>
 
     <footer class="py-12 bg-blue-700 text-center text-white">
      
