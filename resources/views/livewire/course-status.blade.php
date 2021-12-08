b6214199afe78604f65bc26e5de9a70266afe81d
javiercorrea1255 <92273977+javiercorrea1255@users.noreply.github.com>
2021-12-07 19:23:45 -0600

    Update course-status.blade.php

--- resources/views/livewire/course-status.blade.php
+++ resources/views/livewire/course-status.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2792464016414046"
-     crossorigin="anonymous"></script>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Directo al Campo</title>
-</head>
+
 <div>
     <section class="container py-12">
         <div class="grid grid-cols-1 lg:grid-cols-3 gap-8">
